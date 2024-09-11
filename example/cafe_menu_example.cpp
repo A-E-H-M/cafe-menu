09f8612836e8d08d556124ccb68500412a675237
alexandria <alexandria.hmann@gmail.com>
2024-09-11 13:11:12 -0500

    Updated example

--- example/cafe_menu_example.cpp
+++ example/cafe_menu_example.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-#include "cafe_menu_core.hpp"
+//#include "cafe_menu_core.hpp"
 #include "cafe_menu_iostream.hpp"
 
 using namespace cafe_menu;
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
 
 	std::string user_menu {"current_menu.txt"};
 
-	display_menu(set_menu(user_menu));
+	display_menu(create_menu(user_menu));
 
 	return 0;
 
