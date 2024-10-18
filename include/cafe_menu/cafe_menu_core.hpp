5b5b07af27e31d97c0d76da86c5bcfdbc6627c7c
alexandria <alexandria.hmann@gmail.com>
2024-10-18 09:44:47 -0500

    Updated comment

--- include/cafe_menu/cafe_menu_core.hpp
+++ include/cafe_menu/cafe_menu_core.hpp
@@ -47,9 +47,36 @@ namespace cafe_menu
 
 	std::vector<menu_item> create_menu(const std::string& file_path);
 
+	/**
+	 * @brief A calculation function to create a subtotal for the final bill, including
+	 * applying any discounts.
+	 *
+	 * It iterates through two vectors: a vector that has all of customer selected items 
+	 * and a vector of all the items on the current menu. It compares the two vectors to find
+	 * matching items then creates one line item for each matching item by 
+	 * multipling its price by the how many the customer would like to order.
+	 *
+	 * Ex. Price per muffin is $4.25 and customer would like to order 3. The total for that
+	 * line item would be $12.75. No discounts were applied in this example.
+	 *
+	 * @param std::vector<order_item> A std::vector container of CMC order_item structs.
+	 * @param std::vector<menu_item> A std::vector container of CMC menu_item structs.
+	 * @param discounts A CMC struct of discount variables.
+	 *
+	 * @post The return will be a double.
+	 */
 	double calc_sub_total(const std::vector<ordered_item>& customer_order, const std::vector<menu_item>& full_menu, const discounts& order_discounts);
+
+	/**
+	 */
 	double calc_discounts(double sub_total, double discount_rate);
+
+	/**
+	 */
 	double calc_sub_total_tax(double sub_total, double sales_tax_rate);
+
+	/**
+	 */
 	double calc_total(double sub_total, double sub_total_tax);
 	
 }
